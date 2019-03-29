9edff3311caf99b7401cbc6c99637a01f2387598
Daniel Munhoz <danielmmneto@gmail.com>
2019-03-29 11:25:30 -0300

    Finalizado Forms com ReactiveForms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http'
 import { RouterModule } from '@angular/router'
-import { FormsModule } from '@angular/forms'
+import { ReactiveFormsModule } from '@angular/forms'
 import { ROUTES } from './app.routes' 
 
 import { AppComponent } from './app.component';
@@ -38,7 +38,7 @@ import { OrdemCompraSucessoComponent } from './ordem-compra-sucesso/ordem-compra
     BrowserModule,
     HttpModule,
     RouterModule.forRoot(ROUTES),
-    FormsModule
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/ordem-compra/ordem-compra.component.ts
+++ src/app/ordem-compra/ordem-compra.component.ts
@@ -1,40 +1,46 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { NgForm } from '@angular/forms'
+import { Component, OnInit } from '@angular/core';
 import { OrdemCompraService } from '../ordem-compra.service'
 import { Pedido } from '../shared/pedido.model'
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'pur-ordem-compra',
+  selector: 'app-ordem-compra',
   templateUrl: './ordem-compra.component.html',
   styleUrls: ['./ordem-compra.component.css'],
   providers: [ OrdemCompraService ]
 })
 export class OrdemCompraComponent implements OnInit {
 
-  @ViewChild('formulario') public form: NgForm
+  public idPedido: number
 
   constructor(private ordemCompraService: OrdemCompraService) { }
 
-  public idPedido: number
+  public formulario: FormGroup = new FormGroup({
+    'endereco': new FormControl (null, [Validators.required, Validators.minLength(3), Validators.maxLength(15)]),
+    'numero': new FormControl (null, [Validators.required, Validators.minLength(1), Validators.maxLength(6)]),
+    'complemento': new FormControl (null),
+    'formaPagamento': new FormControl (null, [Validators.required])
+  })
 
   ngOnInit() {
     
   }
 
-  public confirmarCompra(formulario: NgForm): void {
-
-    let pedido: Pedido = new Pedido(
-      this.form.value.endereco,
-      this.form.value.numero,
-      this.form.value.complemento,
-      this.form.value.formaPagamento
-    )
-
-    this.ordemCompraService.efetivarCompra(pedido)
-      .subscribe((idPedido: number)=>{
-        console.log('Pedido cadastrado com sucesso! ID do Pedido: ' + idPedido)
-        this.idPedido = idPedido
+  public confirmarCompra(): void {
+    if(this.formulario.status === 'INVALID'){
+      this.formulario.get('endereco').markAsTouched()
+      this.formulario.get('numero').markAsTouched()
+      this.formulario.get('formaPagamento').markAsTouched()
+    }else{
+      let pedido: Pedido = new Pedido(
+        this.formulario.value.endereco,
+        this.formulario.value.numero,
+        this.formulario.value.complemento,
+        this.formulario.value.formaPagamento
+      )
+      this.ordemCompraService.efetivarCompra(pedido).subscribe((idPedido: number)=>{
+        this.idPedido=idPedido
       })
+    }
   }
-
 }
